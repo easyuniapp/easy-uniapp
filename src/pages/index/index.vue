8cb42840070522d10d7815595d7d79b283fc607b
Zone-f <1191066968@qq.com>
2022-05-19 20:52:15 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </div>
+    <el-row>
+    <el-col :sm="12" :lg="6">
+      <el-result
+        icon="success"
+        title="Success Tip"
+        sub-title="Please follow the instructions"
+      >
+        <template #extra>
+          <el-button type="primary">Back</el-button>
+        </template>
+      </el-result>
+    </el-col>
+    <el-col :sm="12" :lg="6">
+      <el-result
+        icon="warning"
+        title="Warning Tip"
+        sub-title="Please follow the instructions"
+      >
+        <template #extra>
+          <el-button type="primary">Back</el-button>
+        </template>
+      </el-result>
+    </el-col>
+    <el-col :sm="12" :lg="6">
+      <el-result
+        icon="error"
+        title="Error Tip"
+        sub-title="Please follow the instructions"
+      >
+        <template #extra>
+          <el-button type="primary">Back</el-button>
+        </template>
+      </el-result>
+    </el-col>
+    <el-col :sm="12" :lg="6">
+      <el-result icon="info" title="Info Tip">
+        <template #sub-title>
+          <p>Using slot as subtitle</p>
+        </template>
+        <template #extra>
+          <el-button type="primary">Back</el-button>
+        </template>
+      </el-result>
+    </el-col>
+  </el-row>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const title = ref('Hello')
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+  margin-top: 200rpx;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 50rpx;
+}
+
+.text-area {
+  display: flex;
+  justify-content: center;
+}
+
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
+}
+</style>
